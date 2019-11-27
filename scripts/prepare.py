ccb9df5953a8572c3931b2791316c8d6e5cae0db
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2019-11-27 11:10:39 +0100

    Finish installation from conda and pip

--- scripts/prepare.py
+++ scripts/prepare.py
@@ -1,6 +1,11 @@
 import config
 import re
+import shutil
+import sys
+import os
+import subprocess
 import importlib
+import conda.cli.python_api as Conda
 from os.path import dirname, join, abspath, exists, isdir, isfile
 
 
@@ -8,12 +13,14 @@ class RequirementsManager:
     _conda_deps = []
     _pip_deps = []
 
+
     def _add_dep(self, list, dep):
         #parse entry: name[==version] [#(bin|py):name-to-check]
-        m=self._regex.search(dep)
+        m = self._regex.search(dep)
         if not m:
             raise Exception(f"Requirement {dep} not in a proper format")
         list.append((m.group(1), m.group(3), m.group(5), m.group(6)))
+
 
     def scan(self, target = None):
         if not target:
@@ -41,7 +48,12 @@ class RequirementsManager:
 
 
     def _verify_binary_dep(self, name, version):
-        pass
+        if shutil.which(name):
+            if version:
+                return version in subprocess.run([name, '--version'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+            return True
+        return False
+
 
     def _verify_python_dep(self, name, version):
         try:
@@ -64,6 +76,26 @@ class RequirementsManager:
         else:
             return self._verify_python_dep(to_check, version)
 
+    def _build_deps_and_run_install(self, params, deps, noun):
+        for dep in deps:
+            if dep[1]: # version
+                params.append(f"{dep[0]}={dep[1]}")
+            else:
+                params.append(dep[0])
+
+        process = subprocess.Popen(params, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+        while True:
+            output = process.stdout.readline()
+            if output == b'' and process.poll() is not None:
+                break
+            if output:
+                print(output.decode(sys.stdout.encoding).strip("\n\r"))
+        if process.poll() == 0:
+            print(f"Succesfully installed {noun} dependencies")
+        else:
+            print(f"There was an error installing {noun} packages, see the log above")
+
+
     def install(self):
         installed_conda = []
         for dep in self._conda_deps:
@@ -78,6 +110,16 @@ class RequirementsManager:
                 installed_pip.append(dep)
 
         self._pip_deps = [x for x in self._pip_deps if x not in installed_pip]
+
+        conda_params = ["conda", "install", "-y"]
+        if "CONDA_FLAGS" in os.environ.keys():
+            conda_params.append(os.environ["CONDA_FLAGS"])
+
+        self._build_deps_and_run_install(conda_params, self._conda_deps, "conda")
+
+        pip_params = ["pip", "install"]
+
+        self._build_deps_and_run_install(pip_params, self._pip_deps, "pip")
 
 
 
@@ -117,4 +159,3 @@ def prepare():
         req.scan(target)
 
     req.install()
-    print("Hello!")
