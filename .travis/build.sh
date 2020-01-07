818377b74f361e6c193cdcfe1f02e1781c6d5cc1
Jan Kowalewski <jkowalewski@antmicro.com>
2020-01-07 13:34:20 +0100

    Refactor build.sh

--- .travis/build.sh
+++ .travis/build.sh
@@ -5,17 +5,16 @@ set -e
 source .travis/common.sh
 
 for target in "${TARGET_CONF[@]}"; do
-	echo "Build: ${C} : ${TC} : ${P} : ${T} : ${F}"
-	if [[ -z $F ]]; then
-		echo "[FIXME] No firmware provided - skipping..."
+	echo -e "${PURPLE}[BUILD] CPU: ${C} | Toolchain: ${TC} | Platform: ${P} | Target: ${T} | Firmware: ${F}${NC}"
+	$SPACER
+	if [[ -z $CPU_VARIANT ]]; then
+		python3 scripts/litex_buildenv_ng.py --cpu $CPU --platform $P --target $target --firmware $F firmware
+		exit $?
+		# [XXX] Add gateware build
 	else
-		if [[ -z $CPU_VARIANT ]]; then
-			python3 scripts/litex_buildenv_ng.py --cpu $CPU --platform $P --target $target --firmware $F firmware
-			exit $?
-		else
-			python3 scripts/litex_buildenv_ng.py --cpu $CPU --cpu-variant $CPU_VARIANT --platform $P --target $target --firmware $F firmware
-			exit $?
-		fi
+		python3 scripts/litex_buildenv_ng.py --cpu $CPU --cpu-variant $CPU_VARIANT --platform $P --target $target --firmware $F firmware
+		# [XXX] Add gateware build
+		exit $?
 	fi
 done
 
