cd635cc8bd377dd55624ed38a884071cca18155f
Jan Kowalewski <jkowalewski@antmicro.com>
2020-01-09 10:16:34 +0100

    Use setup_litex in Travis

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -2,14 +2,7 @@
 
 source .travis/common.sh
 
-LITEX_SETUPS=`find third_party -name "setup.py" | grep "third_party/.*/setup.py"`
-readarray -t LITEX_SETUPS_ARRAY <<< "$LITEX_SETUPS"
-
-for setup in "${LITEX_SETUPS_ARRAY[@]}"; do
-	cd `echo $setup | cut -d"/" -f1-2`
-	python3 setup.py develop
-	cd -
-done
+python3 scripts/setup_litex.py
 
 # Add non-standard configs to default.env configuration file if needed
 # Additional configs should be ';' separated
