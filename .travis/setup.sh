c1353715d1cf0453c173df0db57b260443f9ceb7
Jan Kowalewski <jkowalewski@antmicro.com>
2020-01-10 12:38:37 +0100

    Add GNU tools to macOS build

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -14,3 +14,18 @@ echo "# Additional configs" >> default.env
 for cfg in "${ADDITIONAL_CONFIGS[@]}"; do
 	echo $cfg >> default.env
 done
+
+if [ $TRAVIS_OS_NAME == 'osx' ]; then
+	# Download non-standard elf.h for macOS
+	wget https://gist.githubusercontent.com/mlafeldt/3885346/raw/2ee259afd8407d635a9149fcc371fccf08b0c05b/elf.h
+	cp elf.h /usr/local/include
+
+	# Replace xargs with gxargs
+	ln -s `which gxargs` /usr/local/bin/xargs
+
+	# Replace sed with gsed
+	ln -s `which gsed` /usr/local/bin/sed
+
+	# Replace stat with gstat
+	ln -s `which gstat` /usr/local/bin/stat
+fi
